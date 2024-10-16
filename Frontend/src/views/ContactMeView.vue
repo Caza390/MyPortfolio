d02e638082154f30dd2319356a718c86f0cecde7
Caz <callumcb1@hotmail.co.uk>
2024-10-16 23:51:41 +0100

    Made changes to ContactMeView.vue

--- Frontend/src/views/ContactMeView.vue
+++ Frontend/src/views/ContactMeView.vue
@@ -1,8 +1,27 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+
+const emailAddress = 'callum.carterbegbie@gmail.com';
+const emailLink = computed(() => `mailto:${emailAddress}`);
 </script>
 
 <template>
-  <main>
-    <h1 class="text-white">Hello Contact Me</h1>
+  <main class="lg:py-24 lg:px-36">
+    <p class="text-white text-xl lg:text-3xl">Contact me with either email, phone or discord.
+      I often check my email account and made a discord profile for business use.
+      <br>Personally I prefer to be contacted by these but if you would like to use phone call/text that is also okay -
+      I do
+      have WhatsApp.
+    </p>
+    <p class="text-cz-red-400 text-xl lg:text-3xl my-5">Email : <a :href="emailLink">{{ emailAddress }}</a>
+      <br>
+      Discord : CallumCB
+      <br>
+      Number : 07842 608878
+      <br>
+      LinkedIn : <a href="https://www.linkedin.com/in/callum-carter-begbie-7854bb183/" target="_blank">Callum
+        Carter-Begbie</a>
+    </p>
+    <p class="text-white text-xl lg:text-3xl">I look forward to hearing from you!</p>
   </main>
 </template>
